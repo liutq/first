1fe3e718ac928d563be82debd3f50d4e9b1ff93e
tianqiang <58531102@qq.com>
2018-07-24 15:08:40 +0800

    dev clear

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
- #1111111333333311111111111111 one one one
  #examples
  #this is my test file.
-  # creating a new branch is quick and simple.
 	echo 'this is my first test';
- #creating a new branch is quick & simple.
 ?>
